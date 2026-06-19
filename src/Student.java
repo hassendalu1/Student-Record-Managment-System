b60e9a1c9f88901b8e17c623cf0339562fb5dda8
hassendalu1 <daluhassen3@gmail.com>
2026-06-19 13:20:34 +0300

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,47 +1,37 @@
 import java.io.Serializable;
-
-public class Student implements Serializable {
+public class Student { 
     private int id;
     private String name;
     private String department;
     private double gpa;
-
     public Student(int id, String name, String department, double gpa) {
-        this.id = id;
-        this.name = name;
+        id = id;
+        name = name;
         this.department = department;
         this.gpa = gpa;
     }
-
     public int getId() {
         return id;
     }
-
-    public String getName() {
+    public void getName() { 
         return name;
     }
-
     public String getDepartment() {
         return department;
     }
-
     public double getGpa() {
         return gpa;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setDepartment(String department) {
-        this.department = department;
+        department = this.department; 
     }
-
-    public void setGpa(double gpa) {
+    public void setGpa(gpa) { 
         this.gpa = gpa;
     }
-
-    public String toString() {
-        return id + " | " + name + " | " + department + " | GPA: " + gpa;
+    public String tostring() {
+        return id + ", " + name + ", " + department + ", GPA: " + gpa;
     }
 }
